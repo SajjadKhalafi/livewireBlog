f3ff32b2021b2bce5e306eb3edaab6da6b1a30c4
Sajjad Khalafi <84657147+SajjadKhalafi@users.noreply.github.com>
2022-12-18 13:46:12 +0330

    create posts and show them in table

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Posts extends Component
 {
+    use WithFileUploads;
+    public $user_id , $title , $body , $image;
+    public $showModalForm = false;
+    public function showCreatePostModal()
+    {
+        $this->showModalForm = true;
+    }
+
+    public function storePost()
+    {
+        $this->validate([
+            'title' => ['required'],
+            'body' => ['required'],
+            'image' => ['required' , 'image' , 'max:1024'],
+        ]);
+        $image_name = $this->image->getClientOriginalName();
+        $this->image->storeAs('public/photos', $image_name);
+        $post = new Post();
+        $post->user_id = auth()->user()->id;
+        $post->title = $this->title;
+        $post->slug = Str::slug($this->title);
+        $post->body = $this->body;
+        $post->image = $image_name;
+        $post->save();
+        $this->reset();
+    }
+
     public function render()
     {
-        return view('livewire.posts');
+        return view('livewire.posts' , [
+            'posts' => Post::all()
+        ]);
     }
 }
